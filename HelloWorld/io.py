c2d10dae9f0a38fd9db2376ed0d64a260f3aeefd
李朝阳 <cstlizy@hotmail.com>
2015-01-11 23:40:56 +0800

    python 初涉

---
+++ HelloWorld/io.py
@@ -0,0 +1,7 @@
+# print 'please enter your gendar : '
+# gendar = raw_input()
+# print gendar
+
+name = raw_input('please input your name : ')
+age = raw_input('please input your age : ')
+print 'your name is : ', name, "your age is : ", age
