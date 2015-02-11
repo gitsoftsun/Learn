1278009bd779c82ca3225ae54ed1656eb179ea83
unknown <User19@user19.bdachina.com>
2015-02-11 09:35:21 +0800

    del tes_ide

--- python/Test_ide/test/testForIf.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'lzy'
-# Error : http://www.oschina.net/code/snippet_581482_20892
-values = [1, 2, 3]
-for var in values:
-    if var == 2:
-        print 'I am :', var
-        values.remove(var)
-    print var
